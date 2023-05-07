2cf7aad626b462ef8921db40a1a19a25aa4ee842
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2023-05-07 22:50:32 +0200

    Add install.sh and gitignore

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+APPID="moe.nyarchlinux.tour"
+BUNDLENAME="nyarchtour.flatpak"
+flatpak-builder --install --user --force-clean flatpak-app "$APPID".json
+
+if [ "$1" = "bundle" ]; then
+	flatpak build-bundle ~/.local/share/flatpak/repo "$BUNDLENAME" "$APPID"
+fi
