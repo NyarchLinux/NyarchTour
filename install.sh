3c6f21d0ace3e895881472444e25318c9f99a568
Francesco Caracciolo <67018178+FrancescoCaracciolo@users.noreply.github.com>
2023-05-10 19:02:37 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 APPID="moe.nyarchlinux.tour"
 BUNDLENAME="nyarchtour.flatpak"
-flatpak-builder --install --user --force-clean flatpak-app "$APPID".json
+sudo flatpak-builder --install --force-clean flatpak-app "$APPID".json
 
 if [ "$1" = "bundle" ]; then
 	flatpak build-bundle ~/.local/share/flatpak/repo "$BUNDLENAME" "$APPID"
